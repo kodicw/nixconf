f87813054077ad74641576dbcb4da77289468804
kodicw <111810009+kodicw@users.noreply.github.com>
2023-07-23 09:14:18 -0700

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -3,3 +3,4 @@
 sudo mv /etc/nixos /etc/nixos.backup && \
 sudo git clone -b nadia https://github.com/kodicw/nixconf.git /etc/nixos && \
 sudo cp /etc/nixos.backup/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+sudo nixos-rebuild switch
