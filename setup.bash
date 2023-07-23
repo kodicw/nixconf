6b64ce0128fa6c0731bdb480c3d15805be30f352
kodicw <111810009+kodicw@users.noreply.github.com>
2023-07-23 08:29:22 -0700

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo mv /etc/nixos /etc/nixos.backup && \
-sudo git clone git@github.com:kodicw/nixconf.git /etc/nixos && \
+sudo git clone -b nadia git@github.com:kodicw/nixconf.git /etc/nixos && \
 sudo cp /etc/nixos.backup/hardware-configuration.nix /etc/nixos/configuration.nix
