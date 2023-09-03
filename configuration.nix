d049fd97ee2156aa07d21fd6df782df89c58386a
kodicw <111810009+kodicw@users.noreply.github.com>
2023-09-03 13:20:44 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,9 +8,13 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Bootloader
+boot.loader = {
+  enable = true;
+  device = "/dev/sda";
+  useOSProber = true;
+};
+  
   # Enable opengl
   hardware.opengl = {
     enable = true;
