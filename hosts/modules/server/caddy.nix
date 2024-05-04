29b192105310c594495b75185f2b21117d94dd53
kodicw <kodicw@gmail.com>
2024-05-04 16:18:21 -0700

    caddy

--- hosts/modules/server/caddy.nix
+++ hosts/modules/server/caddy.nix
@@ -4,7 +4,7 @@
     enable = true;
     email = "kodicw@gmail.com";
     virtualHosts = {
-      "virtualpotato.org" = {
+      "virtualpotato.org:80" = {
         extraConfig = ''
         reverse_proxy 127.0.0.1:8082
           '';
