6d7747b305da0e8047a693bd18193714fe73cf26
kodicw <kodicw@gmail.com>
2024-05-04 16:28:50 -0700

    caddy

--- hosts/modules/network/firewall.nix
+++ hosts/modules/network/firewall.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   # networking.firewall.allowedTCPPorts = [ 8000 80 7070 8069 ];
+  networking.firewall.allowedTCPPorts = [443];
   # networking.firewall.allowedUDPPorts = [ 2088 ];
   networking.firewall.enable = true;
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
