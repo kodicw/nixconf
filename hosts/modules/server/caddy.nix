76146d7b4ac6efb8bdb2a18012c7d5be149b28f4
kodicw <kodicw@gmail.com>
2024-05-03 21:15:22 -0700

    caddy

--- hosts/modules/server/caddy.nix
+++ hosts/modules/server/caddy.nix
@@ -3,7 +3,7 @@
   services.caddy = {
     enable = true;
     email = "kodicw@gmail.com";
-    vertualHosts = {
+    virtualHosts = {
       "node-nadia" = {
         extraConfig = ''
         reverse_proxy 127.0.0.1:8082
