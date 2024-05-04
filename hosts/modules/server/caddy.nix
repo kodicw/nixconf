56c07f24fde71a404c4b745b56212e6926c98b51
kodicw <kodicw@gmail.com>
2024-05-04 09:22:26 -0700

    caddy

--- hosts/modules/server/caddy.nix
+++ hosts/modules/server/caddy.nix
@@ -4,7 +4,7 @@
     enable = true;
     email = "kodicw@gmail.com";
     virtualHosts = {
-      "home.node-nadia" = {
+      "node-nadia.kodicw.gmail.com.beta.tailscale.net" = {
         extraConfig = ''
         reverse_proxy 127.0.0.1:8082
           '';
