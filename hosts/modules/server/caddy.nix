f3b71b0dbbc9a477428f7c12b9089efb1c593e54
kodicw <kodicw@gmail.com>
2024-05-04 19:35:58 -0700

    caddy

--- hosts/modules/server/caddy.nix
+++ hosts/modules/server/caddy.nix
@@ -2,6 +2,9 @@
 {
   services.caddy = {
     enable = true;
+    globalConfig = ''
+      get_certificate tailscale
+    '';
     virtualHosts."node-nadia" = {
       extraConfig = ''
         reverse_proxy localhost:8082
