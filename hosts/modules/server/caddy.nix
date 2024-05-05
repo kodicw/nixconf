191f51226d3e16c9ecf918ae8066196d586ce9fe
kodicw <kodicw@gmail.com>
2024-05-04 19:31:45 -0700

    caddy

--- hosts/modules/server/caddy.nix
+++ hosts/modules/server/caddy.nix
@@ -1,25 +1,11 @@
 { config, ...}:
 {
-  services.nginx.virtualHosts."virtualpotato.org" = {
-    forceSSL = true;
-    enableACME = true;
-    locations."/" = {
-        proxyPass = "http://127.0.0.1:8081";
-        proxyWebsockets = true; # needed if you need to use WebSocket
-        extraConfig =
-          # required when the target is also TLS server with multiple hosts
-          "proxy_ssl_server_name on;" +
-          # required when the server wants to use HTTP Authentication
-          "proxy_pass_header Authorization;"
-          ;
-  };
-};
-  
-
-  security.acme = {
-    acceptTerms = true;
-    certs = {
-      "virtualpotato.org".email = "kodicw@gmail.com";
+  services.caddy = {
+    enable = true;
+    virtualHosts."node-nadia" = {
+      extraConfig = ''
+        reverse_proxy localhost:8082
+        '';
+      };
     };
-  };
- }
+}
