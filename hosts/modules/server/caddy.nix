ea6d40d575faadfbe9e8dd3362cacf25074187d1
kodicw <kodicw@gmail.com>
2024-05-04 18:00:22 -0700

    caddy

--- hosts/modules/server/caddy.nix
+++ hosts/modules/server/caddy.nix
@@ -1,15 +1,18 @@
 {conifg, pkgs, ...}:
 {
-  services.caddy = {
+  services.traefik = {
     enable = true;
-    email = "kodicw@gmail.com";
-    virtualHosts = {
-      "virtualpotato.org" = {
-        extraConfig = ''
-        reverse_proxy 192.168.1.12:8082
-          '';
-      };
-    };
   };
-  networking.firewall.allowedTCPPorts = [ 443 80];
+  # services.caddy = {
+  #   enable = true;
+  #   email = "kodicw@gmail.com";
+  #   virtualHosts = {
+  #     "virtualpotato.org" = {
+  #       extraConfig = ''
+  #       reverse_proxy 192.168.1.12:8082
+  #         '';
+  #     };
+  #   };
+  # };
+  # networking.firewall.allowedTCPPorts = [ 443 80];
 }
